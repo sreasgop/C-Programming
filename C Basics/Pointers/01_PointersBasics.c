1ae1165c8dc587c100686a42640ce703ee945d63
sreasgop <sreasgop@gmail.com>
2024-01-09 22:44:22 +0530

    C Basics Dir added

---
+++ C Basics/Pointers/01_PointersBasics.c
@@ -0,0 +1,25 @@
+// POINTER:
+
+// Three parts of a varialbe: 
+// 1. Name
+// 2. Address 
+// 3. Value 
+
+// Pointer is a variable that stores the address of a variable of a certain data type. 
+// Pointer is a derived data type. 
+
+// Syntax of Declaring a Pointer: 
+// datatype * pointer_name
+
+// Example: 
+// int* p;
+
+// Here we cannot simply say that the data type of the p variable is int. A Pointer will contain the address of a variable which is not a value which will be of any datatype like int or float. In this case, p is a pointer vairable that can hold the address of any integer variable. 
+
+
+// Syntax of Initializing a variable: 
+// int a = 10; 
+// int *p = &a; 
+
+// int a = 10; *p = &a;
+
